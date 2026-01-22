5311b767c7afddefc6baab907c36ac1f8c676361
ccho0117 <ccho0117@student.monash.edu>
2026-01-22 15:53:09 +0800

    Change the stack to MERN stack

--- my-app/src/ui/Card.tsx
+++
@@ -1,8 +0,0 @@
-export function Card(props: React.PropsWithChildren<{ className?: string }>) {
-    return (
-      <div className={"rounded-2xl border bg-white shadow-sm " + (props.className ?? "")}>
-        {props.children}
-      </div>
-    );
-  }
-  
